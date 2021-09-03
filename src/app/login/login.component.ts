b0f6441e3984fc1c652c01fda69d2c33eaea9d11
Mersad Hadzic <mersad@bind.ba>
2021-09-03 17:15:51 +0200

    login template

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  title = 'cognito';
+  selection = {
+    value: ''
+  };
+
+  constructor() { }
+
+}
